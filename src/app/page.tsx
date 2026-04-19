55b6e401dc9a99c99a55a960e1d9add2ba9f60c8
Declan Edgecombe <edeclan@amazon.com>
2026-04-18 21:53:58 -0700

    test: trigger webhook with debug logging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main>
       <h1>⚽ World Cup Predictions</h1>
-      <p>Predict the 2026 FIFA World Cup. Coming June 2026.</p>
+      <p>Predict the 2026 FIFA World Cup.</p>
     </main>
   );
 }
